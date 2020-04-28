3817a07ae127adf79975e00d6109b31752e73081
Rui Goncalves <rui.goncalves@talkdesk.com>
2020-04-28 17:58:05 +0100

    Fix error

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.use(express.static('public'))
 
 app.post("/slack/v1/events", function(req, res) {
@@ -13,7 +16,11 @@ app.post("/slack/v1/events", function(req, res) {
     res.json(payload)
 });
 
-var server = app.listen(3000, function() {
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+var server = app.listen(port, function() {
   var host = server.address().address;
   var port = server.address().port;
 
