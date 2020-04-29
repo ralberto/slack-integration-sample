150083258314121733dea27d48a248fae6a3ae93
jmpatrao <jmpatrao@gmail.com>
2020-04-29 10:33:42 +0100

    Update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.post("/slack/v1/events", function(req, res) {
     console.log("=========================================================")
     console.log("Request: %s",req.body.body)
     console.log("=========================================================")
-    console.log("Response: Status %s",res.status)
+    console.log("Response: Status %s",res)
 });
 
 // set the port of our application
