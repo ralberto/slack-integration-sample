9b83c8bb6a6ed00bae9dbad8ddeaf7b546d00bd2
jmpatrao <jmpatrao@gmail.com>
2020-04-30 10:57:04 +0100

    Update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { WebClient } = require('@slack/web-api');
-const slackToken = "xoxb-1078425763427-1090859023793-naSiVnASUI7u7EGDfu4pyf6B";
+const slackToken = "xoxb-1078425763427-1090859023793-bDishs5mdmMsTxQlrx4mHq73";
 
 var express = require("express");
 var bodyParser = require('body-parser');
